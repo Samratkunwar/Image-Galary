35e28e4631fe2ce7ee7ae1c2f6d1114805127b37
Samratkunwar <samratkunwar12@gmail.com>
2019-10-23 00:20:38 +1000

    initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+var PORT = 3000;
+var https = "mongodb/localhost:27017/gallery";
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.listen(PORT, function(){
+    console.log("Server started at port: ", PORT);
+});
