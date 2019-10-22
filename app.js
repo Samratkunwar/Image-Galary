7f55bf3ddf95c770016e62d04bcec9be4fb87d42
Samratkunwar <samratkunwar12@gmail.com>
2019-10-23 00:25:27 +1000

    views

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+// -------------------------- Routes ----------------------------
+
+app.get('/', function(req,res){
+    res.render('home.ejs');
+})
+
+// --------------------------------------------------------------
+
 app.listen(PORT, function(){
     console.log("Server started at port: ", PORT);
 });
