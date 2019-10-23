85ca31575199745f737dc7647258a87083d8616b
Samratkunwar <samratkunwar12@gmail.com>
2019-10-23 21:57:46 +1000

    image grids

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var PORT = 3000;
 var https = "mongodb/localhost:27017/gallery";
+var images = [
+    {name: "Erza", image:"http://www.babyzuzu.com/wp-content/uploads/2019/03/Baby-Born-Low-Key-Photography.jpg"},
+    {name: "John", image:"https://render.fineartamerica.com/images/rendered/default/print/6.000/8.000/break/images/artworkimages/medium/1/beautiful-sensual-portrait-of-a-couple-black-and-white-awen-fine-art-prints.jpg"},
+    {name:"" , image:"https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images/artworkimages/medium/1/romantic-sensual-portrait-of-man-and-woman-embracing-black-and-w-awen-fine-art-prints.jpg"},
+    {name:"", image:"https://i.pinimg.com/originals/d4/91/44/d49144e6f4638f1ca39a0effe0c6371d.jpg"}
+];
 
 var express = require('express');
 var app = express();
@@ -12,7 +18,7 @@ app.set("view engine", "ejs");
 // -------------------------- Routes ----------------------------
 
 app.get('/', function(req,res){
-    res.render('home.ejs');
+    res.render('home', {images:images});
 })
 
 // --------------------------------------------------------------
