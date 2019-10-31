2a33b6e64634f44aa1988832f15c73237ec28199
Samratkunwar <samratkunwar12@gmail.com>
2019-10-31 14:00:38 +1000

    authentication success

--- app.js
+++ app.js
@@ -16,16 +16,33 @@ var LocalStrategy = require('passport-local');
 // importing models class
 var post = require('./models/post.js');
 var Comment = require('./models/comment.js')
-var user = require('./models/user.js')
+var User = require('./models/user.js')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 // connecting the css to html
 app.use(express.static(__dirname + '/public'));
 
+// passport configuration
+app.use(require("express-session")({
+    secret: "You are entering the path of the dragon",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Extracting the current logged in user 
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // setting up a virtual storage
 var Storage = multer.diskStorage({
@@ -145,24 +162,6 @@ app.post('/newimage', function(req,res){
             
         }
     });
-    // var image = fs.readFileSync(req.file.path);
-    // var encoded_image = image.toString('base64');
-    // var image = req.body.image;
-    // var newimage = {
-    //     name:name, 
-    //     message:message, 
-    //     image: new Buffer(encoded_image, 'base64') 
-    // };
-    // post.create(newimage, function(err, newdata){
-    //    if(err){
-    //        console.log(err);
-    //    }
-    //    else{
-    //         console.log('new image added to the collection.');
-    //         console.log(newdata)
-    //         res.redirect("/");
-    //    }
-    // });
 
 });
 
@@ -199,6 +198,48 @@ app.post("/show/:id/comments", function(req, res){
 
 // Route to delete Comment
 
+// ======================================= Routes for user authentication =================================
+
+// route for login
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', passport.authenticate("local", 
+    {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    }) , function(req, res){
+    
+});
+
+//route for registering a new user
+app.get('/register', function(req,res){
+    res.render('register');
+});
+
+app.post('/register', function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        else{
+            passport.authenticate("local")(req, res, function(){
+                console.log("user registration success!");
+                console.log(user);
+                res.redirect('/');
+            });
+        }
+    });
+});
+
+//route for logout
+app.get('/logout', function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
 // --------------------------------------------------------------
 
@@ -207,10 +248,11 @@ app.listen(PORT, function(){
 });
 
 
-/*
-
-1. think of a way to authenticate user
-
-
-*/
+// Authentication function
+function AuthenticateUser(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+};
 
